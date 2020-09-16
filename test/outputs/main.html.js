5e56dcca35b4a6fb8d89168ca23e861ff2f9284a
freeoasoft <freeoasoft@126.com>
2020-09-16 23:13:36 +0800

    update

--- test/outputs/main.html.js
+++ test/outputs/main.html.js
@@ -5,85 +5,143 @@ return (
 	[
 		"box",
 		{
-			"tag": this.id,
-			"style": "height:160rem;margin:20rem 0;overflow:hidden;"
+			"theme": "level1",
+			"style": "margin-top:10rem;padding:0 20rem;"
 		},
 		[
 			[
-				"image",
+				"band",
 				{
-					"src": this.image,
-					"style": "width:200rem;height:100%;"
-				}
-			],
-			[
-				"box",
-				{
-					"style": "display:inline-block;width:500rem;height:100%;padding-left:20rem;"
+					"style": "height:60rem;line-height:60rem;"
 				},
 				[
 					[
-						"band",
+						"icon",
 						{
-							"style": "height:80rem;overflow:hidden;"
+							"theme": "level2",
+							"icon": this.icon,
+							"style": "position:absolute;font-size:50rem;"
+						}
+					],
+					[
+						"text",
+						{
+							"theme": "level2",
+							"text": this.text,
+							"style": "position:absolute;left:60rem;"
+						}
+					],
+					[
+						"box",
+						{
+							"layout": "row",
+							"theme": "level4",
+							"style": "position:absolute;top:0;right:0;bottom:0;font-size:28rem;"
 						},
 						[
 							[
 								"text",
-								{
-									"text": this.name
-								}
-							]
-						]
-					],
-					[
-						"band",
-						{
-							"theme": "level3",
-							"style": "height:40rem;line-height:40rem;font-size:24rem;overflow:hidden;"
-						},
-						(function () {
-
-						    var __for_2_1 = this.remark;
-						    var __for_2_2 = [];
-
-						    for (var index = 0, __for_2_len = __for_2_1.length; index < __for_2_len; index++)
-						    {
-						        var item = __for_2_1[index];
-
-						        __for_2_2.push.apply(__for_2_2,
-									[
-										[
-											"text",
-											{
-												"text": this.item,
-												"theme": "bg-level5",
-												"style": "display:inline-block;border-radius:20rem;padding:0 20rem;margin-right:10rem;"
-											}
-										]
-									] || []);
-						    }
-
-						    return __for_2_2;
-
-						}).call(this)
-					],
-					[
-						"band",
-						{
-							"theme": "important",
-							"style": "height:40rem;overflow:hidden;"
-						},
-						[
+								null,
+								"查看更多"
+							],
 							[
-								"text",
+								"icon",
 								{
-									"text": this.price > 0 ? '￥' + this.price : '免费'
+									"icon": "common-more"
 								}
 							]
 						]
 					]
 				]
+			],
+			[
+				"box",
+				null,
+				(function () {
+
+				    var __for_1_1 = data;
+				    var __for_1_2 = [];
+
+				    for (var index = 0, __for_1_len = __for_1_1.length; index < __for_1_len; index++)
+				    {
+				        var item = __for_1_1[index];
+
+				        __for_1_2.push.apply(__for_1_2,
+							[
+								[
+									"box",
+									{
+										"tag": item.id,
+										"style": "height:160rem;margin:20rem 0;overflow:hidden;"
+									},
+									[
+										[
+											"image",
+											{
+												"src": item.image,
+												"style": "width:200rem;height:100%;"
+											}
+										],
+										[
+											"box",
+											{
+												"style": "display:inline-block;width:500rem;height:100%;padding-left:20rem;"
+											},
+											[
+												[
+													"band",
+													{
+														"style": "height:50rem;overflow:hidden;"
+													},
+													[
+														[
+															"text",
+															{
+																"text": item.name
+															}
+														]
+													]
+												],
+												[
+													"band",
+													{
+														"theme": "level4",
+														"style": "height:70rem;font-size:24rem;overflow:hidden;"
+													},
+													[
+														[
+															"text",
+															{
+																"text": item.remark
+															}
+														]
+													]
+												],
+												[
+													"band",
+													{
+														"theme": "primary",
+														"style": "height:40rem;overflow:hidden;"
+													},
+													[
+														[
+															"text",
+															{
+																"text": '￥' + item.price
+															}
+														]
+													]
+												]
+											]
+										]
+									]
+								]
+							] || []);
+				    }
+
+				    return __for_1_2;
+
+				}).call(this)
 			]
 		]
 	]
