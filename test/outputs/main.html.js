b8fc9fda961f88ab4e3102f49fb25b51aae94d4f
yaozy <yaozy@126.com>
2020-09-25 15:53:50 +0800

    update

--- test/outputs/main.html.js
+++ test/outputs/main.html.js
@@ -4,94 +4,166 @@ if (!$owner) throw new Error("template must input $owner argument! file: d:\\dev
 
 return (
 	[
-		"page",
-		null,
+		"box",
+		{
+			"layout": "column"
+		},
 		[
-			require("../components/header.html")($owner, $data, $model),
 			[
-				"masklayer",
+				require("./search"),
 				{
-					"text": $model.text
+
 				}
 			],
 			[
-				"box",
+				require("./toolbar"),
 				{
-					"width": "550rem",
-					"absolute": "middle center",
-					"padding": "50rem",
-					"theme": "bg-standard"
+
+				}
+			],
+			[
+				"scrollbox",
+				{
+					"flex": "auto",
+					"padding": "0 20rem"
 				},
 				[
 					[
-						"textbox",
+						"databox",
 						{
-							"placeholder": "姓名",
-							"width": "100%",
-							"text-align": "center",
-							"bindings": {
-								"value":  function () { return $model.name },
-								"onchange":  function (value) { $model.name = value; }
-							}
-						}
-					],
-					[
-						"textbox",
-						{
-							"placeholder": "性别",
-							"width": "100%",
-							"text-align": "center",
-							"bindings": {
-								"value":  function () { return $model.gendle },
-								"onchange":  function (value) { $model.gendle = value; }
-							}
-						}
-					],
-					[
-						"textbox",
-						{
-							"placeholder": "电话",
-							"width": "100%",
-							"text-align": "center",
-							"bindings": {
-								"value":  function () { return $model.tel },
-								"onchange":  function (value) { $model.tel = value; }
-							}
-						}
-					],
-					[
-						"textbox",
-						{
-							"placeholder": "地址",
-							"width": "100%",
-							"text-align": "center",
-							"bindings": {
-								"value":  function () { return $model.address },
-								"onchange":  function (value) { $model.address = value; }
-							}
-						}
-					],
-					[
-						"textbox",
-						{
-							"placeholder": "楼宇门牌",
-							"width": "100%",
-							"text-align": "center",
-							"bindings": {
-								"value":  function () { return $model.house },
-								"onchange":  function (value) { $model.house = value; }
-							}
-						}
-					],
-					[
-						"button",
-						{
-							"margin-top": "50rem",
-							"events": {
-								"tap": $owner.handleOK.bind($owner)
-							}
+							"key": "main-body"
 						},
-						"确认"
+						function (template, __data_list, __data_scope) {
+
+							for (var $index = 0, __data_length = __data_list.length; $index < __data_length; $index++)
+							{
+								// 添加作用域解决循环变量绑定变化的问题
+								(function () {
+
+								var $item = __data_list[$index];
+
+								template($index, $item,
+									[
+										"box",
+										{
+											"layout": "line",
+											"tag": $item.id,
+											"height": "160rem",
+											"margin": "20rem 0",
+											"overflow": "hidden",
+											"events": {
+												"tap": $owner.handleOpenDetail.bind($owner)
+											}
+										},
+										[
+											[
+												"image",
+												{
+													"src": $item.image,
+													"width": "200rem",
+													"height": "100%"
+												}
+											],
+											[
+												"box",
+												{
+													"width": "500rem",
+													"height": "100%",
+													"padding-left": "20rem"
+												},
+												[
+													[
+														"box",
+														{
+															"height": "70rem",
+															"overflow": "hidden"
+														},
+														[
+															[
+																"text",
+																{
+																	"bindings": {
+																		"text":  function () { return ("$index" in $item ? $item.$index : $index) + $item.name }
+																	}
+																}
+															]
+														]
+													],
+													[
+														"databox",
+														{
+															"data": $item.remark,
+															"item": "$remark",
+															"index": "$subindex",
+															"layout": "flow",
+															"theme": "text-lightest",
+															"height": "50rem",
+															"line-height": "40rem",
+															"font-size": "24rem",
+															"overflow": "hidden"
+														},
+														function (template, __data_list, __data_scope) {
+
+															var $index = __data_scope[0];
+															var $item = __data_scope[1];
+
+															for (var $subindex = 0, __data_length = __data_list.length; $subindex < __data_length; $subindex++)
+															{
+																// 添加作用域解决循环变量绑定变化的问题
+																(function () {
+
+																var $remark = __data_list[$subindex];
+
+																template($subindex, $remark,
+																	[
+																		"text",
+																		{
+																			"theme": "bg-thick",
+																			"border-radius": "20rem",
+																			"padding": "0 20rem",
+																			"margin-right": "10rem",
+																			"bindings": {
+																				"text":  function () { return ("$index" in $item ? $item.$index : $index) + $remark }
+																			}
+																		}
+																	]
+																);
+
+																})();
+															}
+
+															// end function
+														}
+													],
+													[
+														"box",
+														{
+															"theme": "important",
+															"height": "40rem",
+															"overflow": "hidden"
+														},
+														[
+															[
+																"text",
+																{
+																	"bindings": {
+																		"text":  function () { return $item.price > 0 ? '￥' + $item.price : '免费' }
+																	}
+																}
+															]
+														]
+													]
+												]
+											]
+										]
+									]
+								);
+
+								})();
+							}
+
+							// end function
+						}
 					]
 				]
 			]
